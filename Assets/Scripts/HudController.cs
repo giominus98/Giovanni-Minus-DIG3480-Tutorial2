aa969bd41cb426797a11efb0463f7f9955954b14
giominus98 <giovanni19minus98@gmail.com>
2019-10-25 15:17:04 -0400

    Workshop

---
+++ Assets/Scripts/HudController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HudController : MonoBehaviour
+{
+    public Text HungerText;
+    public Text FriendshipText;
+    public Text IsSickText;
+
+    public GameObject Rock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*HungerText.text = "I'm hungry";
+        FriendshipText.text = "Hello";
+        IsSickText.text = "Healthy";*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HungerText.text = Rock.GetComponent<RockController>().hunger.ToString();
+    }
+}
